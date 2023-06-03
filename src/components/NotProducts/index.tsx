afa70964b032668e2a18763cbae85ecd5b2f5473
0xzay <0xzayeth@gmail.com>
2023-06-03 20:07:03 +0500

    feat: add NotProducts component

---
+++ src/components/NotProducts/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const NotProducts = () => {
+  return (
+    <div className="text-center p-64">
+      <h1 className="text-6xl font-bold">Nothing found 😞</h1>
+      <p className="text-2xl mt-4">
+        Nothing was found for the filters you set try to change them.
+      </p>
+    </div>
+  );
+};
